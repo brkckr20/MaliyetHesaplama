1bc4cf1c835723d51e58b27dcd41fe4c5404867b
brkckr20 <burak2043@gmail.com>
2025-10-03 12:13:46 +0300

    kayıt islemlerine devam edildi.

--- userControls/Uc_MaliyetHesaplama.xaml.cs
+++ userControls/Uc_MaliyetHesaplama.xaml.cs
@@ -6,7 +6,7 @@ namespace MaliyeHesaplama.userControls
 {
     public partial class Uc_MaliyetHesaplama : UserControl
     {
-        int Id, InventoryId, CompanyId, CPIId;
+        int Id, InventoryId, CompanyId, CPIId, CPCId, CCCId;
         bool _receteOlacak = false;
         MiniOrm _orm = new MiniOrm();
         public Uc_MaliyetHesaplama()
@@ -61,13 +61,22 @@ namespace MaliyeHesaplama.userControls
             var dict2 = new Dictionary<string, object>
             {
                 {"Id", CPIId },{"CostId",Id}, {"YI_Warp1", R(txtCozgu1IpBilBolen)}, {"YI_Warp1Divider",R(txtCozgu1IpBilBolunen)}, {"YI_Warp1Result",R(txtCozgu1IpBilSonuc)}, {"YI_Warp2", R(txtCozgu2IpBilBolen)}, {"YI_Warp2Divider",R(txtCozgu2IpBilBolunen)}, {"YI_Warp2Result",R(txtCozgu2IpBilSonuc)}, {"YI_Scarf1", R(txtAtki1IpBilBolen)}, {"YI_Scarf1Divider",R(txtAtki1IpBilBolunen)}, {"YI_Scarf1Result",R(txtAtki1IpBilSonuc)}, {"YI_Scarf2", R(txtAtki2IpBilBolen)}, {"YI_Scarf2Divider",R(txtAtki2IpBilBolunen)}, {"YI_Scarf2Result",R(txtAtki2IpBilSonuc)}, {"YI_Scarf3", R(txtAtki3IpBilBolen)}, {"YI_Scarf3Divider",R(txtAtki3IpBilBolunen)}, {"YI_Scarf3Result",R(txtAtki3IpBilSonuc)}, {"YI_Scarf4", R(txtAtki4IpBilBolen)}, {"YI_Scarf4Divider",R(txtAtki4IpBilBolunen)}, {"YI_Scarf4Result",R(txtAtki4IpBilSonuc)} ,{"D_Warp1", R(txtCozgu1Siklik)},{"D_Warp2", R(txtCozgu2Siklik)},{"D_Scarf1", R(txtAtki1Siklik)},{"D_Scarf2", R(txtAtki2Siklik)},{"D_Scarf3", R(txtAtki3Siklik)},{"D_Scarf4", R(txtAtki4Siklik)},{"WI_CombNo1",R(txtTarakNo1Carpan)},{"WI_CombNo1Multiplier",R(txtTarakNo1Carpim)},{"WI_CombNo1Result",R(txtTarakNo1Sonuc)},{"WI_CombNo2",R(txtTarakNo2Carpan)},{"WI_CombNo2Multiplier",R(txtTarakNo2Carpim)},{"WI_CombNo2Result",R(txtTarakNo2Sonuc)},{"WI_CombWidth",R(txtTarakEn)},{"WI_RawHeight",R(txtHamBoy)},{"WI_HeightEaves",R(txtBoySacakText)},{"WI_WidthEaves",R(txtEnSacakText)},{"WI_RawWidth",R(txtHamEn)},{"WI_ProductHeight",R(txtMamulBoy)},{"WI_ProductWidth",R(txtMamulEn)},{"NW_Warp1",R(txtCozgu1TelSay)},{"NW_Warp2",R(txtCozgu2TelSay)},{"NW_Scarf1",R(txtAtki1TelSay)},{"NW_Scarf2",R(txtAtki2TelSay)},{"NW_Scarf3",R(txtAtki3TelSay)},{"NW_Scarf4",R(txtAtki4TelSay)}
-                // üretim hesaplamadan devam edilecek - 02.10.2025
             };
             CPIId = _orm.Save("CostProductionInformation", dict2);
+            var dict3 = new Dictionary<string, object>
+            {
+                {"Id",CPCId },{"CostId",Id},{"WC_Warp1", txtCozgu1Gramaj.Text},{"WC_Warp2", txtCozgu2Gramaj.Text},{"WC_Scarf1", txtAtki1Gramaj.Text},{"WC_Scarf2", txtAtki2Gramaj.Text},{"WC_Scarf3", txtAtki3Gramaj.Text},{"WC_Scarf4", txtAtki4Gramaj.Text},{"WC_Total", txtToplamGramaj.Text},{"YD_Warp1", R(txtCozgu1IpBoyText)},{"YD_Warp2", R(txtCozgu2IpBoyText)},{"YD_Scarf1", R(txtAtki1IpBoyText)},{"YD_Scarf2", R(txtAtki2IpBoyText)},{"YD_Scarf3", R(txtAtki3IpBoyText)},{"YD_Scarf4", R(txtAtki4IpBoyText)},{"YD_Result", R(txtIpBoySonuc)},{"YP_Warp1", R(txtCozgu1IpFiyText)},{"YP_Warp2", R(txtCozgu2IpFiyText)},{"YP_Scarf1", R(txtAtki1IpFiyText)},{"YP_Scarf2", R(txtAtki2IpFiyText)},{"YP_Scarf3", R(txtAtki3IpFiyText)},{"YP_Scarf4", R(txtAtki4IpFiyText)},{"YC_Warp1", txtCozgu1IpMal.Text},{"YC_Warp2", txtCozgu2IpMal.Text},{"YC_Scarf1", txtAtki1IpMal.Text},{"YC_Scarf2", txtAtki2IpMal.Text},{"YC_Scarf3", txtAtki3IpMal.Text},{"YC_Scarf4", txtAtki4IpMal.Text},{"YC_Result", txtToplamIpMal.Text}
+            };
+            CPCId = _orm.Save("CostProductionCalculate", dict3);
+            var dict4 = new Dictionary<string, object>
+            {
+                {"Id",CCCId },{"CostId",Id},{"PP_Scarf",R(txtAtkiUrFiyText)},{"PP_Warp",R(txtCozguUrFiyText)},{"PP_PartsWashing",R(txtParcaYikamaUrFiyText)},{"PP_FabricWashing",R(txtKumasBoyamaUrFiyText)},{"PP_WeavingWaste",R(txtDokumaFiresiUrFiyText)},{"PP_DyehouseWaster",R(txtBoyaFiresiUrFiyText)},{"PP_GarmentCost",R(txtKonfMaliyetiUrFiyText)},{"PP_2QualityCost",R(txtIkinciKaliyeMaliyetiUrFiyText)},{"PP_Profit",R(txtKarUrFiyText)},{"PP_Vat",R(txtKdvUrFiyText)},{"PP_Currency",R(txtKurUrFiyText)},{"PP_Parity",R(txtPariteUrFiyText)},{"PP_Euro",R(txtEurUrFiyText)},{"WC_Weaving",R(txtDokumaDokMal)},{"WC_Warp",R(txtCozguDokMal)},{"WC_YarnCost",R(txtIplikMaliyetDokMal)},{"WC_Total",R(txtToplamUrMal)},{"WC_Wasted",R(txtFireliUrMal)},{"WC_FRCProfitableForex",R(txtKarliHamKumMal)},{"WC_FRCProfitable",R(txtKarliHamKumMalTL)},{"WDC_PartsWashing",R(txtParcaYikamaYBM)},{"WDC_DyedFabric",R(txtBoyanmisKumasYBM)},{"WDC_DyedFabricTL",R(txtBoyanmisKumasTlYBM)} // dokuma maliyetine kayıt hatası verdi colon bulunamamadı gibi o kontrol edilecek - 03.10.2025
+            };
+            CCCId = _orm.Save("CostCostCalculate", dict4);
             Bildirim.Bilgilendirme2("Veri kayıt işlemi başarıyla gerçekleştirildi");
         }
 
-        string R(TextBox tb) // kayıt esnasında hata alındığı için virgüllü değerler nokta ile değiştirildi. -- replace metorunu kullanıyor
+        string R(TextBox tb) // kayıt esnasında hata alındığı için virgüllü değerler nokta ile değiştirildi. -- replace metodunu kullanıyor
         {
             return tb.Text.Replace(',', '.');
         }
